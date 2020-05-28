9bc347f699a8a4d7071a40ee6509aef5f019b559
Inès <ines.legaud@outlook.com>
2020-05-28 23:27:23 +0200

    add Project

---
+++ src/Entity/Project.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ProjectRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ProjectRepository::class)
+ */
+class Project
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="time")
+     */
+    private $time;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $debutDate;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $leader;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getTime(): ?\DateTimeInterface
+    {
+        return $this->time;
+    }
+
+    public function setTime(\DateTimeInterface $time): self
+    {
+        $this->time = $time;
+
+        return $this;
+    }
+
+    public function getDebutDate(): ?\DateTimeInterface
+    {
+        return $this->debutDate;
+    }
+
+    public function setDebutDate(\DateTimeInterface $debutDate): self
+    {
+        $this->debutDate = $debutDate;
+
+        return $this;
+    }
+
+    public function getLeader(): ?string
+    {
+        return $this->leader;
+    }
+
+    public function setLeader(string $leader): self
+    {
+        $this->leader = $leader;
+
+        return $this;
+    }
+}
